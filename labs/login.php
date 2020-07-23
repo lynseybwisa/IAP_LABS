07751618c4fdec787e5687c5839441f0527701f3
lynseybwisa <lynsey.bwisa@strathmore.edu>
2020-07-23 19:22:19 +0300

    lab 4

--- labs/login.php
+++ labs/login.php
@@ -1,29 +1,28 @@
 <?php
+    include_once 'DBConnector.php';
+    include_once 'User.php';
 
-//lab 2 part 2
-     include_once 'DBConnector.php';
-     include_once 'User.php';
-
-
-      if (isset($_POST['btn-login'])) {
+    // $conn = new DBConnector();
+    if (isset($_POST['btn-login'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
         $instance = User::create();
         $instance->setPassword($password);
         $instance->setUsername($username);
 
-         if ($instance->isPasswordCorrect($username,$password)) {
+        if ($instance->isPasswordCorrect($username,$password)) {
             
             $instance->login();
             // $conn->closeDatabase();
             $instance->createUserSession($username);
         }
-
+        // }else{
+        //     $conn->closeDatabase();
+        //     header('location:login.php');
+        // }
     }
-
-    ?>
-
-    <!doctype html>
+?>
+<!doctype html>
 <html lang="en">
 <head>
 
@@ -36,13 +35,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Trevor Saudi">
     <meta name="generator" content="Jekyll v3.8.5">
-    <title>Login</title>
+    <title>Signin</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sign-in/">
 
-<!-- Bootstrap core CSS -->
+    <!-- Bootstrap core CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    
 
 
@@ -62,12 +61,11 @@
     <!-- Custom styles for this template -->
     <link href="css/signin.css" rel="stylesheet">
 </head>
-
 <body class="text-center">
     <form class="form-signin" method="POST" name="login" id="login" action="<?= $_SERVER['PHP_SELF']?>">
     <div class="container">
-    <div class="row">
-    <div class="section"></div>
+  <div class="row">
+  <div class="section"></div>
    <main>
     <center>
      <div class="container">
@@ -109,7 +107,7 @@
       </center>
       </main>
   
-    </div>
+  </div>
 </div>
          
     </form>
