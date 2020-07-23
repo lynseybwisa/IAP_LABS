a9cb23bf4772739880d44363f425e09064d21a70
lynseybwisa <lynsey.bwisa@strathmore.edu>
2020-07-23 19:23:54 +0300

    lab 4

--- labs/validate.js
+++ labs/validate.js
@@ -1,17 +1,14 @@
 //create a function to validate our form
 //then call the function when form is submitted
 
-function validateForm() {
-	var fname = document.forms ["user_details"]["first_name"].value;
-	var lname = document.forms ["user_details"]["last_name"].value;
-	var city = document.forms ["user_details"] ["city_name"].value;
-
-	//user_details is the name of our form
-
-	if (fname==null || lname=="" || city=="") {
-		alert ("All details required were not supplied");
-		return false;
-	} else {
-return true;
+function validateForm(){
+    var fname = document.forms["user_details"]["first_name"].value;
+    var lname = document.forms["user_details"]["first_name"].value;
+    var city = document.forms["user_details"]["city_name"].value;
+//the user details comes from our form
+if(fname == null || lname == "" || city == ""){
+    alert("all details required were not supplied");
+    return false;
 }
+return true;
 }
