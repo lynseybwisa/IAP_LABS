ee80530ee42d1e39d5e6ea2722e6e2cde8766622
lynseybwisa <lynsey.bwisa@strathmore.edu>
2020-07-23 19:25:23 +0300

    lab 4

---
+++ labs/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Labs IAP</title>
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+<nav>
+    <div class="nav-wrapper">
+    <a href="index.php" class="brand-logo">trevorsaudi</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="lab1.php">Input Table</a></li>
+   
+      </ul>
+    </div>
+  </nav>
+  <div class="section no-pad-bot" id="index-banner">
+    <div class="container">
+      <br><br>
+      <h1 class="header center orange-text">LAB PRACTICALS</h1>
+      <div class="row center">
+        <h5 class="header col s12 light">All weekly labs for Internet Application Programming</h5>
+      </div>
+      <div class="row center">
+        <a href="lab1.php" id="download-button" class="btn-large waves-effect waves-light orange">View Lab 1</a>
+      </div>
+      <br><br>
+    </div>
+  </div>
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+  </body>
+</html>
+    
+</body>
+</html>
