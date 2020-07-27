f4a04e95f127e2d9fffaec4896b64df61aca3fb6
lynseybwisa <lynsey.bwisa@strathmore.edu>
2020-07-27 16:34:42 +0300

    lab 4 complete

--- labs/index.php
+++ labs/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  <title>Labs IAP</title>
+  <title>IAP Labs</title>
   <!-- CSS  -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
@@ -12,9 +12,9 @@
 <body>
 <nav>
     <div class="nav-wrapper">
-    <a href="index.php" class="brand-logo">trevorsaudi</a>
+    <a href="index.php" class="brand-logo">lynsey.bwisa</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="lab1.php">Input Table</a></li>
+        <li><a href="lab1.php">Register User</a></li>
    
       </ul>
     </div>
@@ -22,12 +22,12 @@
   <div class="section no-pad-bot" id="index-banner">
     <div class="container">
       <br><br>
-      <h1 class="header center orange-text">LAB PRACTICALS</h1>
+      <h1 class="header center orange-text">LABS</h1>
       <div class="row center">
-        <h5 class="header col s12 light">All weekly labs for Internet Application Programming</h5>
+        <h5 class="header col s12 light">All labs for Internet Application Programming</h5>
       </div>
       <div class="row center">
-        <a href="lab1.php" id="download-button" class="btn-large waves-effect waves-light orange">View Lab 1</a>
+        <a href="login.php" id="download-button" class="btn-large waves-effect waves-light orange">View Labs</a>
       </div>
       <br><br>
     </div>
